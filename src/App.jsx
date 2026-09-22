9a9ca59e3abc32f27a732e18021b076a3ac7fa64
Amirhossein Esmaeili <mr.unknown85a@gmail.com>
2026-09-22 17:17:16 +0330

    configured graphql and apolloClient

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { gql } from "@apollo/client"
+import { useQuery } from "@apollo/client/react"
 
 function App() {
   return <>hi</>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./styles/index.css"
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
+import { ApolloProvider } from "@apollo/client/react"
 import App from "./App.jsx"
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: import.meta.env.VITE_ENDPOINT,
+  }),
+  cache: new InMemoryCache(),
+})
 
 createRoot(document.getElementById("root")).render(
   //<StrictMode>
-  <App />,
+  <ApolloProvider client={client}>
+    <App />,
+  </ApolloProvider>,
   //</StrictMode>,
 )
